9cbb7e55fcb8376dc785481227462082e0485640
Troy Will <troydwill@gmail.com>
2013-09-04 12:28:17 -0700

    modified:   globalsat-perl.org

--- bin/dbi.pl
+++ bin/dbi.pl
@@ -8,7 +8,7 @@ $dbh = DBI->connect("dbi:Pg:dbname=$dbname", "", "");
 use DBD::Pg qw(:pg_types);
 
 chomp(my $globalsat_data = `./lat-long.pl`);
-my ($time, $lat, $lon, $speed, $track) = split( ' ', $globalsat_data);
+my ($time, $lat, $lon, $speed, $track) = split( ',', $globalsat_data);
 print $time, "\n";
 my $statement = "INSERT INTO locations (time,lat,lon) VALUES ('$time',$lat,$lon)";
 print $statement, "\n";
--- bin/lat-long.pl
+++ bin/lat-long.pl
@@ -1,71 +1,71 @@
 #!/usr/bin/env perl
 
 use strict;
-use warnings;
-use IO::Socket;
-use Time::HiRes qw( sleep );
-use JSON;
-use Math::Round;
-
-$|++;
-
-my $gpsd_socket = new IO::Socket::INET (
-    PeerAddr => 'localhost',
-    PeerPort => '2947',
-    Proto => 'tcp',
-    Blocking => 0
-    ) or die "Could not create socket: $!\n";
-
-banner();
-watch();
-$gpsd_socket->print("?POLL;");
-poll();
-
-exit;
-
-sub watch {
-    $gpsd_socket->print('?WATCH={"enable":true,"json":true}');
-    my $devices_json = <$gpsd_socket>;
-    return unless defined($devices_json);
-    my $devices_hashref = decode_json($devices_json);
-    my @keys = keys %{$devices_hashref};
-    my @values = values %{$devices_hashref};
-    my $watch_json = <$gpsd_socket>;
-}
-
-sub banner {
-    my $banner = <$gpsd_socket>;
-    # {"class":"VERSION","release":"3.7","rev":"3.7","proto_major":3,"proto_minor":7}
-    my $hashref  = decode_json $banner;
-    my @keys = keys %{$hashref};
-    my @values = values %{$hashref};
-    my $release = $hashref->{release};
-}
-
-sub poll {
-    my $count = 1;
-    my $poll_json = <$gpsd_socket>;
-    return unless defined($poll_json);
-    my $hashref  = decode_json $poll_json;
-    my $tpv_hashref = ${$hashref->{tpv}}[0];
-    my ($lat, $lon, $time, $track, $tag, $mode, $speed) = ( $tpv_hashref->{lat}, $tpv_hashref->{lon}, $tpv_hashref->{time}, $tpv_hashref->{track}, $tpv_hashref->{tag}, $tpv_hashref->{mode}, $tpv_hashref->{speed} );
+    use warnings;
+    use IO::Socket;
+    use Time::HiRes qw( sleep );
+    use JSON;
+    use Math::Round;
     
-    print nearest(.00001, $lat), ",", nearest(.00001, $lon), "\n";
-    print "[$time][$track][$speed][$tag][$mode]\n";
-}
-
-while(1)
-{
-#    sleep(1);
-#    print("?POLL;\n");
+    $|++;
+    
+    my $gpsd_socket = new IO::Socket::INET (
+        PeerAddr => 'localhost',
+        PeerPort => '2947',
+        Proto => 'tcp',
+        Blocking => 0
+        ) or die "Could not create socket: $!\n";
+    
+    banner();
+    watch();
     $gpsd_socket->print("?POLL;");
-    while(<$gpsd_socket>)
-    {
-        print;
+    poll();
+    
+    exit;
+    
+    sub watch {
+        $gpsd_socket->print('?WATCH={"enable":true,"json":true}');
+        my $devices_json = <$gpsd_socket>;
+        return unless defined($devices_json);
+        my $devices_hashref = decode_json($devices_json);
+        my @keys = keys %{$devices_hashref};
+        my @values = values %{$devices_hashref};
+        my $watch_json = <$gpsd_socket>;
     }
-}
-
-close($gpsd_socket);
-# 34.08244,-117.72159
-# tag, class, climb, speed, device, track, alt, mode
-# epx, ept, epv, eps, epy
+    
+    sub banner {
+        my $banner = <$gpsd_socket>;
+        # {"class":"VERSION","release":"3.7","rev":"3.7","proto_major":3,"proto_minor":7}
+        my $hashref  = decode_json $banner;
+        my @keys = keys %{$hashref};
+        my @values = values %{$hashref};
+        my $release = $hashref->{release};
+    }
+    
+    sub poll {
+        my $count = 1;
+        my $poll_json = <$gpsd_socket>;
+        return unless defined($poll_json);
+        my $hashref  = decode_json $poll_json;
+        my $tpv_hashref = ${$hashref->{tpv}}[0];
+        my ($lat, $lon, $time, $track, $tag, $mode, $speed) = ( $tpv_hashref->{lat}, $tpv_hashref->{lon}, $tpv_hashref->{time}, $tpv_hashref->{track}, $tpv_hashref->{tag}, $tpv_hashref->{mode}, $tpv_hashref->{speed} );
+        
+        print "$time,",  nearest(.00001, $lat), ",", nearest(.00001, $lon), "\n";
+#        print "[$time][$track][$speed][$tag][$mode]\n";
+    }
+    
+    while(1)
+    {
+    #    sleep(1);
+    #    print("?POLL;\n");
+        $gpsd_socket->print("?POLL;");
+        while(<$gpsd_socket>)
+        {
+            print;
+        }
+    }
+    
+    close($gpsd_socket);
+    # 34.08244,-117.72159
+    # tag, class, climb, speed, device, track, alt, mode
+    # epx, ept, epv, eps, epy
