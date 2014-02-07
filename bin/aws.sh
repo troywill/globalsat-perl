4124bc4433ab26607a5e67f68df428254c1ad160
Troy Will <troydwill@gmail.com>
2014-02-07 10:38:24 -0800

    modified:   ../aws-postgresql.org

--- bin/aws.sh
+++ bin/aws.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+set -o nounset
 
 host="ec2-54-215-211-54.us-west-1.compute.amazonaws.com"
+host="ec2-54-215-227-49.us-west-1.compute.amazonaws.com"
+user="ec2-user"
+private_key="~/.ssh/keypair.pem"
+private_key="/home/troy/.ssh/keypair.pem"
 
-ssh -i ~/.ssh/perl.pem ec2-user@$host
-ssh -i ~/.ssh/perl.pem ec2-user@54.215.211.54
-
-
-
-
+ssh -i ${private_key} ${user}@${host}
+ssh -i ${private_key} ${user}@54.215.211.54
